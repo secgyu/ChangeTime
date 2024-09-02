7ea4747d5632b6fa7e2317fda8043cc147e7b7fa
secgyu <abc2232002@gmail.com>
2024-09-03 01:44:25 +0900

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [minute, setMinute] = useState(); 
+  const [hour, setHour] = useState(); 
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleMinuteChange = (event) => {
+    setMinute(Number(event.target.value));
+  };
+
+  const handleHourChange = (event) => {
+    setHour(Number(event.target.value));
+  };
+
+  const convertTime = () => {
+    if (isFlipped) {
+      setMinute(hour * 60); 
+    } else {
+      setHour(minute / 60); 
+    }
+  };
+
+  const flipConversion = () => {
+    setIsFlipped(!isFlipped);
+    if (isFlipped) {
+      setHour(minute / 60);
+    } else {
+      setMinute(hour * 60);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="number" value={minute} onChange={handleMinuteChange} disabled={isFlipped} />
+      minutes
+      <br />
+      <input type="number" value={hour} onChange={handleHourChange} disabled={!isFlipped} />
+      hours
+      <p />
+      <button onClick={convertTime} style={{marginRight : "10px"}}>변환</button>
+      <button onClick={flipConversion}>방향 전환</button>
+      <div>Direction: {isFlipped ? "Hours to Minutes" : "Minutes to Hours"}</div>
     </div>
   );
 }
